81e19d192838bfe03518cf4c719eb1e11b0d4c0d
Abhishek Anand <anand2000ys@gmail.com>
2023-02-04 12:11:35 +0530

    Initial Commit

---
+++ src/views/Bindings.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="home">
+        <!-- <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+
+        <h1>Hello World</h1>
+        <!-- 2 way binding -->
+        <input type="text" v-model="user">
+        <!-- 1 way binding -->
+        <h1 class="myClass">Hey!! {{ user }}</h1>
+    </div>
+</template>
+
+<script>
+
+export default {
+    data() {
+        return {
+            //let user = " "
+            user: "Abhi"
+        }
+    },
+}
+</script>
+
+<style>
+.myClass {
+    color: red;
+}
+</style>
---
+++ src/views/Methods.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <button @click="sayHello()">Click Me</button>
+        <h1>You pressed button {{ counter }} times</h1>
+    </div>
+</template>
+
+<script>
+
+export default {
+    data() {
+        return {
+            counter: 0
+        }
+    },
+    methods: {
+        sayHello() {
+            this.counter++;
+            console.log("Hello");
+        }
+    },
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
